c4acf48652e1676c6ec31f55b611bd85051fde2e
UN-9BOT <un.9bot@gmail.com>
2023-04-05 15:17:24 +0300

    dox comments for stack.c

--- src/stack.c
+++ src/stack.c
@@ -1,5 +1,13 @@
 #include "stack.h"
 
+/**
+ * @brief init stack with void params
+ *
+ * @warning wtf = -1 -- that mean it is first elem and not use this elem in prog
+ *
+ * 
+ * @return first elem in stack
+ */
 t_stack* initSt() {
     t_stack* st = calloc(1, sizeof(t_stack));
     st->lexem[0] = ' ';
@@ -9,6 +17,16 @@ t_stack* initSt() {
     return (st);
 }
 
+/**
+ * @brief add new elem on top stack
+ *
+ * @param st - latest elem in stack
+ * @param wtf - what is it
+ * @param num 
+ * @param lexem
+ *
+ * @return elem on top stack 
+ */
 t_stack* pushSt(t_stack* st, int wtf, int num, char *lexem) {
     t_stack* new_st = calloc(1, sizeof(t_stack));
     strcpy(new_st->lexem, lexem);
@@ -19,6 +37,17 @@ t_stack* pushSt(t_stack* st, int wtf, int num, char *lexem) {
     return (new_st);
 }
 
+/**
+ * @brief pop last elem on top stack
+ * @warning memory is freed from under the elem we take away
+ *
+ * @param st
+ * @param wtf
+ * @param num
+ * @param lexem[5]
+ *
+ * @return elem under pop elem
+ */
 t_stack* popSt(t_stack* st, int *wtf, int *num, char lexem[5]) {
     strcpy(lexem, st->lexem);
     *num = st->num;
@@ -28,6 +57,12 @@ t_stack* popSt(t_stack* st, int *wtf, int *num, char lexem[5]) {
     return(temp);
 }
 
+/**
+ * @brief destroy and free all memory 
+ * @warning this func need for test_modules
+ *
+ * @param st
+ */
 void destroySt(t_stack* st) {
     while (st->wtf != -1) {
         t_stack* temp = st->prev;
